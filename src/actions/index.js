3a656b541e337e31b270f5c8b707d4f7c55e8be0
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-18 13:11:00 -0400

    pulling to macbook

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,27 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
-	return async function(dispatch, getState) {
+	return async (dispatch) => {
 		const response = await jsonPlaceholder.get('/posts');
 		dispatch({
 			type: 'FETCH_POSTS',
-			payload: response
+			payload: response.data
 		});
 	};
 };
+
+export const fetchUser = (id) => async (dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+	dispatch({
+		type: 'FETCH_USER',
+		payload: response.data
+	});
+};
+
+// export const fetchPosts = () => async (dispatch) => {
+//   const response = await jsonPlaceholder.get('/posts');
+//   dispatch({
+//     type: 'FETCH_POSTS',
+//     payload: response
+//   });
+// };
