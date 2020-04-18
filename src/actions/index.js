ef005bf6088540853417650ef38248aed11c6caa
chase lindsay <chase.lindsay1@gmail.com>
2020-04-18 14:14:57 -0400

    refactored with lodash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,17 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+	const userIds = _.uniq(_.map(getState().posts, 'userId'));
+	userIds.forEach((id) => dispatch(fetchUser(id)));
+
+	// _.chain(getState().posts)
+	// .map('userId')
+	// .unique()
+	// .forEach(id => dispatch(fetchUser(id)))
+	// .value()
+};
 
 export const fetchPosts = () => {
 	return async (dispatch) => {
